0d8de8e50e4abd5d5a76cba789fd32d66e893dd1
ImPanyAngel <angelides.pany04@gmail.com>
2024-11-02 15:17:28 +0000

    filter

--- src/Components/FilterColumn.jsx
+++ src/Components/FilterColumn.jsx
@@ -1,8 +1,43 @@
+import searchIcon from '../assets/search-icon.png';
+
 function FilterColumn() {
+    const items = ['Optiojkewn 1', 'Option 2', 'Option 3', 'Option 4'];
+
 
     return(
         <div className="filter-container">
-            <input className="search-input" placeholder="Search"></input>
+            <div className="search-container">
+                <input className="search-input" placeholder="Search..."></input>
+                <button className="search-button"><img className='search-button-img' src={searchIcon} alt='search'/></button>
+            </div>
+            <div className='courses-container'>
+                <p className='section-title'>Courses...</p>
+                <div className="checkbox-list">
+                    {items.map((item) => (
+                        <label key={item} className="checkbox-item">
+                            {item}
+                            <input
+                                type="checkbox"
+                                value={item}
+                            />
+                        </label>
+                    ))}
+                </div>
+            </div>
+            <div className='skills-container'>
+                <p className='section-title'>Skills...</p>
+                <div className="checkbox-list">
+                    {items.map((item) => (
+                        <label key={item} className="checkbox-item">
+                            {item}
+                            <input
+                                type="checkbox"
+                                value={item}
+                            />
+                        </label>
+                    ))}
+                </div>
+            </div>
         </div>
     );
 }
