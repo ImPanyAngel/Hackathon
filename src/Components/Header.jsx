3e27cb9546a404af0444ab2da0af7eb710418178
ImPanyAngel <angelides.pany04@gmail.com>
2024-11-02 19:04:25 +0000

    post button

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,7 +1,15 @@
+import { useState } from 'react';
+
 import logo from '../assets/logo.png';
 import title from '../assets/CojectLogo.png';
+import closeIcon from '../assets/close-icon.png';
 
 function Header() {
+    const [isPopupVisible, setIsPopupVisible] = useState(false);
+
+    const togglePopup = () => {
+        setIsPopupVisible(!isPopupVisible);
+    };
 
     return(
         <div className="header-container">
@@ -10,9 +18,26 @@ function Header() {
                 <img className='title' src={title} alt='title' />
             </div>
             <div className='header-button-container'>
-                <button className='post-button'>+ Post</button>
+                <button className='post-button' onClick={togglePopup}>+ Post</button>
                 <button className='login-button'>Login</button>
             </div>
+
+            {isPopupVisible && (
+                <div className="popup-overlay">
+                    <div className="popup">
+                        <div className='close-container'>
+                            <img className='close-popup' onClick={togglePopup} src={closeIcon} alt='close'/>
+                        </div>
+                        <p className='popup-title'>Add a new post</p>
+                        <input className="popup-input" type='text' placeholder='Enter project title' />
+                        <input className="popup-input" type='text' placeholder='Enter your course' />
+                        <input className="popup-input" type='text' placeholder='Enter skills required (separated by ",")' />
+                        <input className="popup-input" type='text' placeholder='Enter project description' />
+                        <button className="confirm-popup-btn" onClick={togglePopup}>Confirm</button>
+                    </div>
+                </div>
+            )}
+
         </div>
     );
 }
