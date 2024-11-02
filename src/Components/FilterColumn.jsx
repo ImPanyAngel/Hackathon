167ec0b1cc4c368fe484eb6894217831734e569f
ImPanyAngel <angelides.pany04@gmail.com>
2024-11-02 14:06:09 +0000

    filter

--- src/Components/FilterColumn.jsx
+++ src/Components/FilterColumn.jsx
@@ -2,6 +2,7 @@ function FilterColumn() {
 
     return(
         <div className="filter-container">
+            <input className="search-input" placeholder="Search"></input>
         </div>
     );
 }
