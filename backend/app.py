245d3f7ed17a7f2f32dac3c27c651f93b24ffa4d
Rahul Ganesh Sawant <rahulsawant8433@gmail.com>
2024-11-03 12:01:28 +0000

    Recommendation Part

--- backend/app.py
+++ backend/app.py
@@ -1,24 +1,51 @@
 import sqlite3
 from flask_cors import CORS
 from flask import Flask, jsonify, request
+import pickle
+import pandas as pd
 
 app = Flask(__name__)
 CORS(app)
+
+
+
+ #product
+with open(r"C:\Users\rahul\OneDrive\Desktop\last\Hackathon\backend\postList.pkl","rb") as f:
+    prod = pickle.load(f)
+    df =pd.DataFrame(prod)
+
+with open(r"C:\Users\rahul\OneDrive\Desktop\last\Hackathon\backend\similarity.pkl","rb") as f:
+    similarity_mat= pickle.load(f)
+
+
+
+def post_recommend(course):
+    postIndex  = df[df['course']=='Biology'].index[0]
+    distances_ = similarity_mat[postIndex]
+    post_list = sorted(list(enumerate(distances_)),reverse=True,key=lambda x:x[1])[1:10]
+    recommended_df = df.iloc[[i[0] for i in post_list]]
+    return recommended_df
+
+
+
+
+
 def get_db_connection():
     try:
-        conn = sqlite3.connect(r'Sqlite3.db')
+        conn = sqlite3.connect(r'C:\Users\rahul\OneDrive\Desktop\Hackathon\data\Sqlite3.db')
         conn.row_factory = sqlite3.Row
         return conn
     except sqlite3.Error as error:
         print("Database connection error:", error)
         return None
+    
 
 @app.route('/getPosts', methods=['GET'])
 def getPosts():
     conn = get_db_connection()
     if conn is None:
         return jsonify({"error": "Database connection failed"}), 500
-
+    
     try:
         cursor = conn.cursor()
         posts = [dict(row) for row in cursor.execute("SELECT * FROM posts")]
@@ -31,12 +58,14 @@ def getPosts():
     finally:
         conn.close()
 
+
+
 @app.route('/getCourse', methods=['GET'])
 def getCourse():
     conn = get_db_connection()
     if conn is None:
         return jsonify({"error": "Database connection failed"}), 500
-
+    
     try:
         cursor = conn.cursor()
         posts = [dict(row) for row in cursor.execute("SELECT DISTINCT course FROM posts")]
@@ -55,7 +84,7 @@ def getSkills():
     conn = get_db_connection()
     if conn is None:
         return jsonify({"error": "Database connection failed"}), 500
-
+    
     try:
         cursor = conn.cursor()
         posts = [dict(row) for row in cursor.execute("SELECT DISTINCT skills FROM posts")]
@@ -67,3 +96,15 @@ def getSkills():
 
     finally:
         conn.close()
+
+
+@app.route('/getRecommendations', methods=['GET'])
+def getRecommendations():
+    course = request.args.get('course', default="Biology")
+    recommendations = post_recommend(course)
+    
+    if recommendations is None:
+        return jsonify({"error": "Course not found"}), 404
+    
+    
+    return jsonify(recommendations.to_dict(orient="records"))
